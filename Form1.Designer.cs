a907d2a8c09be79271554e65f96599ad4a71859f
Bellezatabaskena <114634159+Bellezatabaskena@users.noreply.github.com>
2023-01-18 19:20:47 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace LogInUniversidadDelSur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblWarning = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.txbxUsername = new System.Windows.Forms.TextBox();
+            this.txbxPassword = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(303, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(339, 375);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Universidad del Sur";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password:";
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.lblWarning.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWarning.Location = new System.Drawing.Point(20, 258);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(2, 26);
+            this.lblWarning.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(31, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 47);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(144, 342);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(89, 45);
+            this.buttonExit.TabIndex = 6;
+            this.buttonExit.Text = "EXIT";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // txbxUsername
+            // 
+            this.txbxUsername.Location = new System.Drawing.Point(135, 110);
+            this.txbxUsername.Name = "txbxUsername";
+            this.txbxUsername.Size = new System.Drawing.Size(162, 20);
+            this.txbxUsername.TabIndex = 7;
+            // 
+            // txbxPassword
+            // 
+            this.txbxPassword.Location = new System.Drawing.Point(135, 182);
+            this.txbxPassword.Name = "txbxPassword";
+            this.txbxPassword.PasswordChar = '*';
+            this.txbxPassword.Size = new System.Drawing.Size(162, 20);
+            this.txbxPassword.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::LogInUniversidadDelSur.Properties.Resources.IMG_0593;
+            this.ClientSize = new System.Drawing.Size(659, 403);
+            this.Controls.Add(this.txbxPassword);
+            this.Controls.Add(this.txbxUsername);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblWarning;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.TextBox txbxUsername;
+        private System.Windows.Forms.TextBox txbxPassword;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogInUniversidadDelSur
+{
+    public partial class Form1 : Form
+    {
+        static int attempts = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //define username and password
+            //username="Perla" and password ="Ladybug1"
+            //you have only 3 attempts
+            string username = txbxUsername.Text;
+            string password = txbxPassword.Text;
+            
+
+                if (this.txbxUsername.Text == "Perla" && this.txbxPassword.Text == "Ladybug1")
+                {
+                    attempts = 0;
+                    pictureBox1.Image = new Bitmap(@"C:\Users\perla\source\repos\LogInUniversidadDelSur\unlocklogo.png");
+                    MessageBox.Show("Welcome");
+
+                }
+                else if (attempts <= 3 && attempts > 0)
+                {
+                  
+                    //warns the user of the number  of attempts left
+                    lblWarning.Text = "You have " + Convert.ToString(attempts-1) + " attempts left";
+                    attempts--;
+                    lblWarning.BackColor = Color.Red;
+                    pictureBox1.Image = new Bitmap(@"C:\Users\perla\source\repos\LogInUniversidadDelSur\warning.png");
+                   
+                }
+                else
+                {
+                    MessageBox.Show("WRONG USERNAME AND PASSWORD");
+                    attempts++;
+                    MessageBox.Show("You have exceeded the maximum number of attempts. Good bye!");
+                    Application.Exit();
+
+                 }
+                
+
+
+
+        }
+    }
+}
